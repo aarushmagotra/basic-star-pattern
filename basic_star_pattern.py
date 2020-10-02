4d688f59fe1d8efc1830ffbc47bd7fe827b86852
KalaHit <72244776+KalaHit@users.noreply.github.com>
2020-10-02 16:24:44 +0000

    Create basic_star_pattern.py

---
+++ basic_star_pattern.py
@@ -0,0 +1,8 @@
+# This program will print a simple star pattern
+a = int(input('Enter a number: '))
+for i in range(1, a+1):
+    for j in range(1, i+1):
+        print('*', end = ' ')
+    print()
+    
+# here there will be a code that will print the same pattern in reverse
